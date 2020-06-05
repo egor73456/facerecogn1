e1d499acac6cd2a82a0627c24b32be723a46aecb
egor73456 <tigerboy22@rambler.ru>
2020-06-05 20:19:04 +0300

    add .vscode to determine how to launch properly

--- src/main1.cpp
+++ src/main1.cpp
@@ -1,18 +1,22 @@
 #include "lib.hpp"
 
-int main(int argc, char const **argv) {
-  if (argc != 3 && argc != 1) {
+int main(int argc, char const **argv)
+{
+  if (argc != 3 && argc != 1)
+  {
     std::cerr << "Usage: " << argv[0]
-              << " [\"caffe/config/path\" \"caffe/weights/path\"]" << std::endl;
+              << " [\"/caffe/config/path\" \"/caffe/weights/path\"]" << std::endl;
     exit(1);
   }
-  if (argc == 3) {
+  if (argc == 3)
+  {
     caffeConfigFile = argv[1];
     caffeWeightFile = argv[2];
   }
   int hue = 100, sat = 100, val = 100;
   int inverse_colors = 0;
-  cv::Mat drawing_layer, frame, text_n_boxes;;
+  cv::Mat drawing_layer, frame, text_n_boxes;
+  ;
   cv::namedWindow("Video", cv::WINDOW_AUTOSIZE);
   BreakThroughContainer args{drawing_layer, frame, 0, 0};
   cv::setMouseCallback("Video", mouseHandler, reinterpret_cast<void *>(&args));
@@ -25,11 +29,16 @@ int main(int argc, char const **argv) {
   // cv::QT_PUSH_BUTTON);
   std::atomic<bool> stop_flag = 0;
   cv::dnn::Net net;
-  try {
+  try
+  {
     net = cv::dnn::readNetFromCaffe(caffeConfigFile, caffeWeightFile);
-  } catch (std::exception) {
+  }
+  catch (std::exception)
+  {
     std::cerr << "[" << argv[0] << "] Файл(ы) не найден(ы) или поврежден(ы)!"
               << std::endl;
+
+    std::cerr << argv[1] << " " << argv[2] << std::endl;
     exit(1);
   }
   cv::VideoCapture capture{0};
@@ -41,7 +50,8 @@ int main(int argc, char const **argv) {
   std::thread detect_th{[&net, &frame, &text_n_boxes, &stop_flag] {
     detectFaceOpenCVDNN(net, frame, text_n_boxes, stop_flag);
   }};
-  for (;;) {
+  for (;;)
+  {
     cv::Mat tmp;
     {
       std::lock_guard a{main_im_access};
@@ -57,10 +67,12 @@ int main(int argc, char const **argv) {
     auto diff = (curt_time = std::chrono::system_clock::now()) - prev_time;
     if (diff.count() > 0)
       fps = 1'000 / (std::chrono::duration_cast<std::chrono::milliseconds>(diff).count());
-    if (fps <= fps_max) {
+    if (fps <= fps_max)
+    {
       cv::imshow("Video", tmp);
       prev_time = curt_time;
-    } else
+    }
+    else
       fps = fps_max;
 
     auto key = cv::waitKey(1);
